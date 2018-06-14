89165758f61ecba13f35440d88e876001da7db1b
Peter Edge <pedge@uber.com>
2018-06-14 17:13:31 -0400

    Address comments

--- internal/protoc/downloader.go
+++ internal/protoc/downloader.go
@@ -110,15 +110,11 @@ func (d *downloader) cache() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	downloaded, err := d.isDownloaded(basePath)
-	if err != nil {
-		return "", err
-	}
-	if !downloaded {
+	if d.checkDownloaded(basePath) != nil {
 		if err := d.download(basePath); err != nil {
 			return "", err
 		}
-		if _, err := d.isDownloaded(basePath); err != nil {
+		if err := d.checkDownloaded(basePath); err != nil {
 			return "", err
 		}
 		d.logger.Debug("protobuf downloaded", zap.String("path", basePath))
@@ -130,23 +126,24 @@ func (d *downloader) cache() (string, error) {
 	return basePath, nil
 }
 
-func (d *downloader) isDownloaded(basePath string) (bool, error) {
+func (d *downloader) checkDownloaded(basePath string) error {
 	buffer := bytes.NewBuffer(nil)
 	cmd := exec.Command(filepath.Join(basePath, "bin", "protoc"), "--version")
 	cmd.Stdout = buffer
 	if err := cmd.Run(); err != nil {
-		return false, nil
+		return err
 	}
 	if d.protocURL != "" {
 		// skip version check since we do not know the version
-		return true, nil
+		return nil
 	}
 	output := strings.TrimSpace(buffer.String())
 	d.logger.Debug("output from protoc --version", zap.String("output", output))
-	if output != fmt.Sprintf("libprotoc %s", d.config.Compile.ProtobufVersion) {
-		return false, nil
+	expected := fmt.Sprintf("libprotoc %s", d.config.Compile.ProtobufVersion)
+	if output != expected {
+		return fmt.Errorf("expected %s from protoc --version, got %s", expected, output)
 	}
-	return true, nil
+	return nil
 }
 
 func (d *downloader) download(basePath string) (retErr error) {
