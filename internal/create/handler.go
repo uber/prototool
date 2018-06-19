040d20f8b95bc9aefd92736c59202d78de461ad7
Peter Edge <pedge@uber.com>
2018-06-19 18:52:49 +0200

    Basics of create working without deriving the package

--- internal/create/handler.go
+++ internal/create/handler.go
@@ -21,9 +21,31 @@
 package create
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
 	"github.com/uber/prototool/internal/settings"
 	"go.uber.org/zap"
 )
+
+var tmpl = template.Must(template.New("tmpl").Parse(`syntax = "proto3";
+
+package {{.Pkg}};
+
+option go_package = "{{.GoPkg}}";
+option java_package = "{{.JavaPkg}}";`))
+
+type tmplData struct {
+	Pkg     string
+	GoPkg   string
+	JavaPkg string
+}
 
 type handler struct {
 	logger         *zap.Logger
@@ -46,6 +68,11 @@ func newHandler(options ...HandlerOption) *handler {
 
 func (h *handler) Create(filePaths ...string) error {
 	for _, filePath := range filePaths {
+		if err := h.checkFilePath(filePath); err != nil {
+			return err
+		}
+	}
+	for _, filePath := range filePaths {
 		if err := h.create(filePath); err != nil {
 			return err
 		}
@@ -53,6 +80,60 @@ func (h *handler) Create(filePaths ...string) error {
 	return nil
 }
 
-func (h *handler) create(filePath string) error {
+func (h *handler) checkFilePath(filePath string) error {
+	if filePath == "" {
+		return errors.New("filePath empty")
+	}
+	dirPath := filepath.Dir(filePath)
+
+	fileInfo, err := os.Stat(dirPath)
+	if err != nil {
+		return err
+	}
+	if !fileInfo.IsDir() {
+		return fmt.Errorf("%q is not a directory somehow", dirPath)
+	}
+	if _, err := os.Stat(filePath); err == nil {
+		return fmt.Errorf("%q already exists", filePath)
+	}
 	return nil
+}
+
+func (h *handler) create(filePath string) error {
+	pkg, err := h.getPkg(filePath)
+	if err != nil {
+		return err
+	}
+	data, err := getData(
+		&tmplData{
+			Pkg:     pkg,
+			GoPkg:   getGoPkg(pkg),
+			JavaPkg: getJavaPkg(pkg),
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filePath, data, 0644)
+}
+
+func (h *handler) getPkg(filePath string) (string, error) {
+	return DefaultPackage, nil
+}
+
+func getGoPkg(pkg string) string {
+	split := strings.Split(pkg, ".")
+	return split[len(split)-1] + "pb"
+}
+
+func getJavaPkg(pkg string) string {
+	return "com." + pkg + ".pb"
+}
+
+func getData(tmplData *tmplData) ([]byte, error) {
+	buffer := bytes.NewBuffer(nil)
+	if err := tmpl.Execute(buffer, tmplData); err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
 }
