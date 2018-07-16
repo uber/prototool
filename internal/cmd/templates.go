de180aa28f13c8b48bcc9a2f9589e0c90042fa16
Peter Edge <pedge@uber.com>
2018-07-16 10:58:59 +0200

    gRPC

--- internal/cmd/templates.go
+++ internal/cmd/templates.go
@@ -221,6 +221,86 @@ If Vim integration is set up, files will be generated when you open a new Protob
 	grpcCmdTemplate = &cmdTemplate{
 		Use:   "grpc dirOrProtoFiles...",
 		Short: "Call a gRPC endpoint. Be sure to set required flags address, method, and either data or stdin.",
+		Long: `What this does behind the scenes:
+
+- Compiles your Protobuf files with "protoc", generating a "FileDescriptorSet".
+- Uses the "FileDescriptorSet" to figure out the request and response type for
+  the endpoint, and to convert the JSON input to binary.
+- Calls the gRPC endpoint.
+- Uses the "FileDescriptorSet" to convert the resulting binary back to JSON,
+  and prints it out for you.
+
+All these steps take on the order of milliseconds, for example the overhead for a file with four dependencies is about 30ms, so there is little overhead for CLI calls to gRPC.
+
+There is a full example for gRPC in the example directory of Prototool. Run "make init example" to make sure everything is installed and generated.
+
+Start the example server in a separate terminal by doing "go run example/cmd/excited/main.go".
+
+prototool grpc dirOrProtoFiles... \
+  --address serverAddress \
+  --method package.service/Method \
+  --data 'requestData'
+
+Either use "--data 'requestData'" as the the JSON data to input, or "--stdin" which will result in the input being read from stdin as JSON.
+
+$ make init example # make sure everything is built just in case
+
+$ cat input.json
+{"value":"hello"}
+
+$ cat input.json | prototool grpc example \
+  --address 0.0.0.0:8080 \
+  --method foo.ExcitedService/Exclamation \
+  --stdin
+{
+  "value": "hello!"
+}
+
+$ cat input.json | prototool grpc example \
+  --address 0.0.0.0:8080 \
+  --method foo.ExcitedService/ExclamationServerStream \
+  --stdin
+{
+  "value": "h"
+}
+{
+  "value": "e"
+}
+{
+  "value": "l"
+}
+{
+  "value": "l"
+}
+{
+  "value": "o"
+}
+{
+  "value": "!"
+}
+
+$ cat input.json
+{"value":"hello"}
+{"value":"salutations"}
+
+$ cat input.json | prototool grpc example \
+  --address 0.0.0.0:8080 \
+  --method foo.ExcitedService/ExclamationClientStream \
+  --stdin
+{
+  "value": "hellosalutations!"
+}
+
+$ cat input.json | prototool grpc example \
+  --address 0.0.0.0:8080 \
+  --method foo.ExcitedService/ExclamationBidiStream \
+  --stdin
+{
+  "value": "hello!"
+}
+{
+  "value": "salutations!"
+}`,
 		Run: func(runner exec.Runner, args []string, flags *flags) error {
 			return runner.GRPC(args, flags.headers, flags.address, flags.method, flags.data, flags.callTimeout, flags.connectTimeout, flags.keepaliveTime, flags.stdin)
 		},
