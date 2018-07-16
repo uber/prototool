0f95d781d45ccac0de3ba1f398a9d1d97bb461f3
Peter Edge <pedge@uber.com>
2018-07-16 10:43:57 +0200

    Start work on longer help docs

--- internal/cmd/templates.go
+++ internal/cmd/templates.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -71,6 +72,7 @@ var (
 	compileCmdTemplate = &cmdTemplate{
 		Use:   "compile dirOrProtoFiles...",
 		Short: "Compile with protoc to check for failures.",
+		Long:  `Stubs will not be generated. To generate stubs, use the "gen" command. Calling "compile" has the effect of calling protoc with "-o /dev/null".`,
 		Run: func(runner exec.Runner, args []string, flags *flags) error {
 			return runner.Compile(args, flags.dryRun)
 		},
@@ -135,6 +137,7 @@ var (
 	formatCmdTemplate = &cmdTemplate{
 		Use:   "format dirOrProtoFiles...",
 		Short: "Format a proto file and compile with protoc to check for failures.",
+		Long:  `By default, the values for "java_multiple_files", "java_outer_classname", and "java_package" are updated to reflect what is expected by the Google Cloud APIs file structure at https://cloud.google.com/apis/design/file_structure, and the value of "go_package" is updated to reflect what we expect for the default Style Guide. By formatting, the linting for these values will pass by default. See the documentation for "create" for an example. This functionality can be suppressed by passing the flag "--no-rewrite".`,
 		Run: func(runner exec.Runner, args []string, flags *flags) error {
 			return runner.Format(args, flags.overwrite, flags.diffMode, flags.lintMode, !flags.noRewrite)
 		},
@@ -180,6 +183,7 @@ var (
 	initCmdTemplate = &cmdTemplate{
 		Use:   "init [dirPath]",
 		Short: "Generate an initial config file in the current or given directory.",
+		Long:  `All available options will be generated, but all options except "protoc_version" :will be commented out unless the --uncomment flag is specified.`,
 		Args:  cobra.MaximumNArgs(1),
 		Run: func(runner exec.Runner, args []string, flags *flags) error {
 			return runner.Init(args, flags.uncomment)
@@ -204,6 +208,7 @@ var (
 	lintCmdTemplate = &cmdTemplate{
 		Use:   "lint dirOrProtoFiles...",
 		Short: "Lint proto files and compile with protoc to check for failures.",
+		Long:  `The default rule set follows the Style Guide at https://github.com/uber/prototool/blob/master/etc/style/uber/uber.proto. You can add or exclude lint rules in your "prototool.yaml" file. The default rule set is very strict and is meant to enforce consistent development patterns.`,
 		Run: func(runner exec.Runner, args []string, flags *flags) error {
 			return runner.Lint(args, flags.listAllLinters, flags.listLinters)
 		},
@@ -294,9 +299,9 @@ type cmdTemplate struct {
 func (c *cmdTemplate) Build(exitCodeAddr *int, stdin io.Reader, stdout io.Writer, stderr io.Writer, flags *flags) *cobra.Command {
 	command := &cobra.Command{}
 	command.Use = c.Use
-	command.Short = c.Short
+	command.Short = strings.TrimSpace(c.Short)
 	if c.Long != "" {
-		command.Long = fmt.Sprintf("%s\n%s", c.Short, c.Long)
+		command.Long = fmt.Sprintf("%s\n\n%s", strings.TrimSpace(c.Short), strings.TrimSpace(c.Long))
 	}
 	command.Args = c.Args
 	command.Run = func(_ *cobra.Command, args []string) {
