acb79dd8c837b001f59c6194462a34fe6f044e9a
Peter Edge <peter.edge@gmail.com>
2018-08-17 00:45:45 +0200

    Fix reference to vars package in releasegen.sh and Makefile (#190)

--- etc/bin/releasegen.sh
+++ etc/bin/releasegen.sh
@@ -39,7 +39,7 @@ for os in Darwin Linux; do
       go build \
       -a \
       -installsuffix cgo \
-      -ldflags "-X 'github.com/uber/prototool/internal/x/vars.GitCommit=$(git rev-list -1 HEAD)' -X 'github.com/uber/prototool/internal/x/vars.BuiltTimestamp=$(date -u)'" \
+      -ldflags "-X 'github.com/uber/prototool/internal/vars.GitCommit=$(git rev-list -1 HEAD)' -X 'github.com/uber/prototool/internal/vars.BuiltTimestamp=$(date -u)'" \
       -o "${dir}/bin/prototool" \
       internal/cmd/prototool/main.go
     tar -C "${tar_context_dir}" -cvzf "${BASE_DIR}/prototool-${os}-${arch}.tar.gz" "${tar_dir}"
