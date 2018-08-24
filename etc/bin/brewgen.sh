7784323e7fb28ed7e77105f4a808e4911936eeb2
Peter Edge <pedge@uber.com>
2018-08-24 18:05:21 +0200

    Fix zsh location for /usr/local and glide install in etc/bin/releasegen.sh

--- etc/bin/brewgen.sh
+++ etc/bin/brewgen.sh
@@ -12,10 +12,10 @@ glide install
 rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}/bin"
 mkdir -p "${BUILD_DIR}/etc/bash_completion.d"
-mkdir -p "${BUILD_DIR}/etc/zsh_completion.d"
+mkdir -p "${BUILD_DIR}/etc/zsh/site-functions"
 mkdir -p "${BUILD_DIR}/share/man/man1"
 go run internal/cmd/gen-prototool-bash-completion/main.go > "${BUILD_DIR}/etc/bash_completion.d/prototool"
-go run internal/cmd/gen-prototool-zsh-completion/main.go > "${BUILD_DIR}/etc/zsh_completion.d/prototool"
+go run internal/cmd/gen-prototool-zsh-completion/main.go > "${BUILD_DIR}/etc/zsh/site-functions/prototool"
 go run internal/cmd/gen-prototool-manpages/main.go "${BUILD_DIR}/share/man/man1"
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 \
   go build \
