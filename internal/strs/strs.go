b090973b250cd37b53411176a42ba5ec31ccc26c
Peter Edge <pedge@uber.com>
2018-12-11 17:28:48 -0800

    Fix issue with ToUpperSnakeCase where an abbreviation may come after an existing word

--- internal/strs/strs.go
+++ internal/strs/strs.go
@@ -188,7 +188,7 @@ func IsUppercase(s string) bool {
 func toSnake(s string) string {
 	output := ""
 	for i, c := range s {
-		if i > 0 && isUpper(c) && output[len(output)-1] != '_' && i < len(s)-1 && !isUpper(rune(s[i+1])) {
+		if i > 0 && isUpper(c) && output[len(output)-1] != '_' && ((i < len(s)-1 && !isUpper(rune(s[i+1]))) || (isLower(rune(s[i-1])))) {
 			output += "_" + string(c)
 		} else {
 			output += string(c)
