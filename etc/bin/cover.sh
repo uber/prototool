4996a0b649c93046d0942a5fc5f1094469a9db5b
Peter Edge <pedge@uber.com>
2018-12-19 15:49:32 -0500

    go.mod

--- etc/bin/cover.sh
+++ etc/bin/cover.sh
@@ -5,11 +5,6 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
 cd "${DIR}"
 
-if echo "${GOPATH}" | grep : >/dev/null; then
-	echo "error: GOPATH must be one directory, but has multiple directories separated by colons: ${GOPATH}" >&2
-	exit 1
-fi
-
 if ! which jq > /dev/null; then
   echo "error: jq must be installed to run code coverage" >&2
   exit 1
@@ -23,46 +18,9 @@ if [[ -d "$COVER" ]]; then
 fi
 mkdir -p "$COVER"
 
-ignorePkgs=""
-
-filterIgnorePkgs() {
-  if [[ -z "${ignorePkgs}" ]]; then
-    cat
-  else
-    grep -v "${ignorePkgs}"
-  fi
-}
-
-# If a package directory has a .nocover file, don't count it when calculating
-# coverage.
-filter=""
-for pkg in "$@"; do
-	if [[ -f "$GOPATH/src/$pkg/.nocover" ]]; then
-		if [[ -n "$filter" ]]; then
-			ignorePkgs="$ignorePkgs\|"
-			filter="$filter, "
-		fi
-		ignorePkgs="$ignorePkgs$pkg/"
-		filter="$filter\"$pkg\": true"
-	fi
-done
-
 i=0
-commands_file="$(mktemp)"
-echo 'commands:' >> "${commands_file}"
-trap 'rm -rf "${commands_file}"' EXIT
 for pkg in "$@"; do
-	if ! ls "${GOPATH}/src/${pkg}" | grep _test\.go$ >/dev/null; then
-		continue
-	fi
 	i=$((i + 1))
-
-	extracoverpkg=""
-	if [[ -f "$GOPATH/src/$pkg/.extra-coverpkg" ]]; then
-		extracoverpkg=$( \
-			sed -e "s|^|$pkg/|g" < "$GOPATH/src/$pkg/.extra-coverpkg" \
-			| tr '\n' ',')
-	fi
 
 	coverpkg=$(go list -json "$pkg" | jq -r '
 		.Deps + .TestImports + .XTestImports
@@ -71,13 +29,9 @@ for pkg in "$@"; do
 		| map
 			( select(startswith("'"$ROOT_PKG"'"))
 			| select(contains("/vendor/") | not)
-			| select({'"$filter"'}[.] | not)
 			)
 		| join(",")
 	')
-	if [[ -n "$extracoverpkg" ]]; then
-		coverpkg="$extracoverpkg$coverpkg"
-	fi
 
 	args=""
 	if [[ -n "$coverpkg" ]]; then
@@ -89,11 +43,4 @@ for pkg in "$@"; do
 done
 
 rm -f coverage.txt
-
-# Merge cross-package coverage and then split the result into main and
-# experimental coverages.
-#
-# We ignore packages in the form "footest" and any mock files.
-gocovmerge "$COVER"/*.out \
-	| filterIgnorePkgs \
-	> coverage.txt
+gocovmerge "$COVER"/*.out > coverage.txt
