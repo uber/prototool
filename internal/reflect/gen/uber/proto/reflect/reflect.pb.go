912db7b6b61a347954e29b6c1f924db3a5740f7a
Peter Edge <pedge@uber.com>
2019-01-02 13:24:37 -0500

    More work

--- internal/reflect/gen/uber/proto/reflect/reflect.pb.go
+++ internal/reflect/gen/uber/proto/reflect/reflect.pb.go
@@ -18,12 +18,64 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
+// PackageSet is a set of Packages.
+type PackageSet struct {
+	// packages contains the set of Packages.
+	//
+	// These will all be unique, and can be referenced by their name field.
+	Packages             []*Package `protobuf:"bytes,1,rep,name=packages,proto3" json:"packages,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}   `json:"-"`
+	XXX_unrecognized     []byte     `json:"-"`
+	XXX_sizecache        int32      `json:"-"`
+}
+
+func (m *PackageSet) Reset()         { *m = PackageSet{} }
+func (m *PackageSet) String() string { return proto.CompactTextString(m) }
+func (*PackageSet) ProtoMessage()    {}
+func (*PackageSet) Descriptor() ([]byte, []int) {
+	return fileDescriptor_reflect_834152692011e8d4, []int{0}
+}
+func (m *PackageSet) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PackageSet.Unmarshal(m, b)
+}
+func (m *PackageSet) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PackageSet.Marshal(b, m, deterministic)
+}
+func (dst *PackageSet) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PackageSet.Merge(dst, src)
+}
+func (m *PackageSet) XXX_Size() int {
+	return xxx_messageInfo_PackageSet.Size(m)
+}
+func (m *PackageSet) XXX_DiscardUnknown() {
+	xxx_messageInfo_PackageSet.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PackageSet proto.InternalMessageInfo
+
+func (m *PackageSet) GetPackages() []*Package {
+	if m != nil {
+		return m.Packages
+	}
+	return nil
+}
+
 // Package describes a Protobuf package, constructed from a set of Protobuf
 // files that have the same "package" value.
 type Package struct {
-	// name is the fully-qualified name of the package. This does not include
-	// the prefix '.' found in the traditional package fully-qualified name.
-	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// name is the fully-qualified name of the package.
+	//
+	// The package name can be thought of as the unique identifier for a
+	// Package message, and is used as a reference.
+	//
+	// This does not include the prefix '.' found in the traditional package
+	// fully-qualified name.
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// dependency_names contains the names of this packages' dependencies.
+	//
+	// If you have a set of Package messages, this will correspond to the name
+	// field of other Packages in the set.
+	DependencyNames      []string `protobuf:"bytes,2,rep,name=dependency_names,json=dependencyNames,proto3" json:"dependency_names,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -33,7 +85,7 @@ func (m *Package) Reset()         { *m = Package{} }
 func (m *Package) String() string { return proto.CompactTextString(m) }
 func (*Package) ProtoMessage()    {}
 func (*Package) Descriptor() ([]byte, []int) {
-	return fileDescriptor_reflect_3a13b01e2f0b953f, []int{0}
+	return fileDescriptor_reflect_834152692011e8d4, []int{1}
 }
 func (m *Package) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Package.Unmarshal(m, b)
@@ -60,22 +112,34 @@ func (m *Package) GetName() string {
 	return ""
 }
 
+func (m *Package) GetDependencyNames() []string {
+	if m != nil {
+		return m.DependencyNames
+	}
+	return nil
+}
+
 func init() {
+	proto.RegisterType((*PackageSet)(nil), "uber.proto.reflect.PackageSet")
 	proto.RegisterType((*Package)(nil), "uber.proto.reflect.Package")
 }
 
 func init() {
-	proto.RegisterFile("uber/proto/reflect/reflect.proto", fileDescriptor_reflect_3a13b01e2f0b953f)
+	proto.RegisterFile("uber/proto/reflect/reflect.proto", fileDescriptor_reflect_834152692011e8d4)
 }
 
-var fileDescriptor_reflect_3a13b01e2f0b953f = []byte{
-	// 116 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_reflect_834152692011e8d4 = []byte{
+	// 179 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x28, 0x4d, 0x4a, 0x2d,
 	0xd2, 0x2f, 0x28, 0xca, 0x2f, 0xc9, 0xd7, 0x2f, 0x4a, 0x4d, 0xcb, 0x49, 0x4d, 0x2e, 0x81, 0xd1,
-	0x7a, 0x60, 0x51, 0x21, 0x21, 0x90, 0x0a, 0x08, 0x5b, 0x0f, 0x2a, 0xa3, 0x24, 0xcb, 0xc5, 0x1e,
-	0x90, 0x98, 0x9c, 0x9d, 0x98, 0x9e, 0x2a, 0x24, 0xc4, 0xc5, 0x92, 0x97, 0x98, 0x9b, 0x2a, 0xc1,
-	0xa8, 0xc0, 0xa8, 0xc1, 0x19, 0x04, 0x66, 0x3b, 0x19, 0x73, 0x89, 0x25, 0xe7, 0xe7, 0xea, 0x61,
-	0x6a, 0x74, 0xe2, 0x09, 0x82, 0x30, 0x02, 0x40, 0xa2, 0x01, 0x8c, 0x51, 0x9c, 0x50, 0x89, 0x82,
-	0xa4, 0x24, 0x36, 0xb0, 0x4a, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0xe9, 0xf9, 0x32, 0x98,
-	0x92, 0x00, 0x00, 0x00,
+	0x7a, 0x60, 0x51, 0x21, 0x21, 0x90, 0x0a, 0x08, 0x5b, 0x0f, 0x2a, 0xa3, 0xe4, 0xca, 0xc5, 0x15,
+	0x90, 0x98, 0x9c, 0x9d, 0x98, 0x9e, 0x1a, 0x9c, 0x5a, 0x22, 0x64, 0xce, 0xc5, 0x51, 0x00, 0xe1,
+	0x15, 0x4b, 0x30, 0x2a, 0x30, 0x6b, 0x70, 0x1b, 0x49, 0xeb, 0x61, 0x6a, 0xd2, 0x83, 0xea, 0x08,
+	0x82, 0x2b, 0x56, 0xf2, 0xe0, 0x62, 0x87, 0x0a, 0x0a, 0x09, 0x71, 0xb1, 0xe4, 0x25, 0xe6, 0xa6,
+	0x4a, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0x81, 0xd9, 0x42, 0x9a, 0x5c, 0x02, 0x29, 0xa9, 0x05,
+	0xa9, 0x79, 0x29, 0xa9, 0x79, 0xc9, 0x95, 0xf1, 0x20, 0xa1, 0x62, 0x09, 0x26, 0x05, 0x66, 0x0d,
+	0xce, 0x20, 0x7e, 0x84, 0xb8, 0x1f, 0x48, 0xd8, 0xc9, 0x98, 0x4b, 0x2c, 0x39, 0x3f, 0x17, 0x8b,
+	0xad, 0x4e, 0x3c, 0x41, 0x10, 0x46, 0x00, 0x48, 0x34, 0x80, 0x31, 0x8a, 0x13, 0x2a, 0x51, 0x90,
+	0x94, 0xc4, 0x06, 0x56, 0x69, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x51, 0xf7, 0x17, 0xfb, 0x04,
+	0x01, 0x00, 0x00,
 }
