aa3d05b61f2498ff480e7e449fa48311faf265ea
Peter Edge <pedge@uber.com>
2019-02-11 13:02:00 -0500

    More testing

--- docker/testing/bin/test.sh
+++ docker/testing/bin/test.sh
@@ -7,7 +7,7 @@ cd "${DIR}"
 
 check_command_success() {
   echo "Checking that '${*}' is successful"
-  if ! "${@}" >/dev/null; then
+  if ! "${@}"; then
     echo "Expected '${*}' to be successful but had error" >&2
     exit 1
   fi
@@ -24,6 +24,14 @@ check_command_output() {
 
 check_which() {
   check_command_output "${1}" command -v "$(basename "${1}")"
+}
+
+check_dir_not_exists() {
+  echo "Checking that '${1}' does not exist"
+  if [ -d "${1}" ]; then
+    echo "Expected '${1}' to not exist" >&2
+    exit 1
+  fi
 }
 
 check_which /usr/bin/protoc
@@ -48,8 +56,11 @@ check_which /usr/local/bin/protoc-gen-twirp_python
 check_which /usr/local/bin/protoc-gen-yarpc-go
 check_which /usr/local/bin/prototool
 check_command_output "libprotoc 3.6.1" protoc --version
-check_command_success protoc -o /dev/null $(find proto -name '*.proto')
+check_command_success protoc -I /usr/local/include -I proto -o /dev/null $(find proto -name '*.proto')
+check_command_success rm -rf gen
 check_command_success prototool compile proto
 check_command_success prototool lint proto
 check_command_success prototool format -l proto
 check_command_success prototool generate proto
+check_command_success rm -rf gen
+check_dir_not_exists /root/.cache
