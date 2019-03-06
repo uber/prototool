63a4f395347b492f2e0cecb69c6fe019b949c56b
Peter Edge <pedge@uber.com>
2019-03-05 19:42:19 -0500

    Upgrade rules_go and gazelle

--- etc/bin/bazelgen.sh
+++ etc/bin/bazelgen.sh
@@ -5,39 +5,41 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${0}")/../.." && pwd)"
 cd "${DIR}"
 
-rm -f WORKSPACE
-rm -f bazel/deps.bzl
+init_workspace() {
+cat << EOF > WORKSPACE
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
-TMPDIR="$(mktemp -d)"
-trap 'rm -rf "${TMPDIR}"' EXIT
+http_archive(
+    name = "io_bazel_rules_go",
+    urls = ["https://github.com/bazelbuild/rules_go/releases/download/0.18.0/rules_go-0.18.0.tar.gz"],
+    sha256 = "301c8b39b0808c49f98895faa6aa8c92cbd605ab5ad4b6a3a652da33a1a2ba2e",
+)
+http_archive(
+    name = "bazel_gazelle",
+    urls = ["https://github.com/bazelbuild/bazel-gazelle/releases/download/0.17.0/bazel-gazelle-0.17.0.tar.gz"],
+    sha256 = "3c681998538231a2d24d0c07ed5a7658cb72bfb5fd4bf9911157c0e9ac6a2687",
+)
+
+load("@io_bazel_rules_go//go:deps.bzl", "go_rules_dependencies", "go_register_toolchains")
+go_rules_dependencies()
+go_register_toolchains()
+
+load("@bazel_gazelle//:deps.bzl", "gazelle_dependencies")
+gazelle_dependencies()
+EOF
+}
+
+rm -f WORKSPACE
+rm -rf bazel
+
+init_workspace
 
 mkdir -p bazel
-
 touch bazel/BUILD.bazel
-
 cat << EOF > bazel/deps.bzl
 load("@bazel_gazelle//:deps.bzl", "go_repository")
 
 def prototool_deps(**kwargs):
-EOF
-
-cat << EOF > WORKSPACE
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "io_bazel_rules_go",
-    urls = ["https://github.com/bazelbuild/rules_go/releases/download/0.17.1/rules_go-0.17.1.tar.gz"],
-    sha256 = "6776d68ebb897625dead17ae510eac3d5f6342367327875210df44dbe2aeeb19",
-)
-http_archive(
-    name = "bazel_gazelle",
-    urls = ["https://github.com/bazelbuild/bazel-gazelle/releases/download/0.16.0/bazel-gazelle-0.16.0.tar.gz"],
-    sha256 = "7949fc6cc17b5b191103e97481cf8889217263acf52e00b560683413af204fcb",
-)
-load("@io_bazel_rules_go//go:deps.bzl", "go_rules_dependencies", "go_register_toolchains")
-go_rules_dependencies()
-go_register_toolchains()
-load("@bazel_gazelle//:deps.bzl", "gazelle_dependencies")
-gazelle_dependencies()
 EOF
 
 go mod tidy -v
@@ -51,45 +53,9 @@ FIRST_GO_REPOSITORY_LINE_NUMBER="$(grep -n ^go_repository WORKSPACE | head -1 | 
 tail -n "+${FIRST_GO_REPOSITORY_LINE_NUMBER}" WORKSPACE | grep -v ^$ | sed 's/^\(.*\)$/    \1/' >> bazel/deps.bzl
 
 rm -f WORKSPACE
-cat << EOF > WORKSPACE
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-
-http_archive(
-    name = "io_bazel_rules_go",
-    urls = ["https://github.com/bazelbuild/rules_go/releases/download/0.17.1/rules_go-0.17.1.tar.gz"],
-    sha256 = "6776d68ebb897625dead17ae510eac3d5f6342367327875210df44dbe2aeeb19",
-)
-
-http_archive(
-    name = "bazel_gazelle",
-    urls = ["https://github.com/bazelbuild/bazel-gazelle/releases/download/0.16.0/bazel-gazelle-0.16.0.tar.gz"],
-    sha256 = "7949fc6cc17b5b191103e97481cf8889217263acf52e00b560683413af204fcb",
-)
-
-# TODO: remove when fixed in io_bazel_rules_go
-load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
-git_repository(
-    name = "com_github_golang_protobuf",
-    remote = "https://github.com/golang/protobuf",
-    commit = "c823c79ea1570fb5ff454033735a8e68575d1d0f",
-    patches = [
-        "@io_bazel_rules_go//third_party:com_github_golang_protobuf-gazelle.patch",
-        "@io_bazel_rules_go//third_party:com_github_golang_protobuf-extras.patch",
-    ],
-    patch_args = ["-p1"],
-)
-
-load("@io_bazel_rules_go//go:deps.bzl", "go_rules_dependencies", "go_register_toolchains")
-
-go_rules_dependencies()
-
-go_register_toolchains()
-
-load("@bazel_gazelle//:deps.bzl", "gazelle_dependencies", "go_repository")
-
-gazelle_dependencies()
+init_workspace
+cat << EOF >> WORKSPACE
 
 load("//bazel:deps.bzl", "prototool_deps")
-
 prototool_deps()
 EOF
